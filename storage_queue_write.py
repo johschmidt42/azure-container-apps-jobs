c0f186dd8b2339a0022b42ea5f42157da4f6e1ca
johschmidt42 <johannes.schmidt.vik@gmail.com>
2024-05-22 07:46:37 +0200

    refactor: Split write_queue() into multiple functions
    The write_queue() function in storage_queue_write.py has been refactored to increase readability and modularity. This function has been split into three separate functions: generate_order_msg(), write_to_queue(content), and write_queue(). Each function now has a single responsibility, which improves the maintainability and testing capabilities of the code.

--- storage_queue_write.py
+++ storage_queue_write.py
@@ -3,7 +3,7 @@ import sys
 import uuid
 
 from azure.identity import DefaultAzureCredential
-from azure.storage.queue import QueueClient
+from azure.storage.queue import QueueClient, QueueMessage
 
 from models import OrderEvent, StorageQueueSettings
 
@@ -12,27 +12,56 @@ logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 logger.level = logging.INFO
 
 
+def generate_order_msg() -> str:
+    """
+    Generate an order message.
+
+    Returns:
+        str: The JSON representation of the order event.
+    """
+    order_id: str = str(uuid.uuid4())
+    logger.info(f"Generating a new order msg: {order_id}")
+    order_event: OrderEvent = OrderEvent(order_id=order_id, msg="Hello World")
+    return order_event.model_dump_json()
+
+
+def write_to_queue(content: str) -> None:
+    """
+    Args:
+        content (str): The content to be written to the queue.
+
+    Returns:
+        None
+
+    """
+    settings: StorageQueueSettings = StorageQueueSettings()
+    credential: DefaultAzureCredential = DefaultAzureCredential()
+    queue_client: QueueClient = QueueClient(
+        settings.storage_queue_url,
+        queue_name=settings.queue_name,
+        credential=credential,
+    )
+    msg: QueueMessage = queue_client.send_message(content=content, time_to_live=-1)
+    logger.info(msg)
+
+
 def write_queue() -> None:
+    """
+    Writes the content generated from the `generate_order_msg()` method to a queue using the `write_to_queue()` method, and logs any exceptions that occur.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If an error occurs while writing to the queue or generating the order message.
+
+    """
     try:
-        order_id: str = str(uuid.uuid4())
-        logger.info(f"Writing a new message to storage queue: {order_id}")
-
-        settings: StorageQueueSettings = StorageQueueSettings()
-        credential = DefaultAzureCredential()
-        queue_client: QueueClient = QueueClient(
-            settings.storage_queue_url,
-            queue_name=settings.queue_name,
-            credential=credential,
-        )
-
-        content: str = OrderEvent(
-            order_id=order_id, msg="Hello World"
-        ).model_dump_json()
-        msg: str = queue_client.send_message(content=content, time_to_live=-1)
-        logger.info(msg)
-        return
+        content: str = generate_order_msg()
+        write_to_queue(content=content)
     except Exception as ex:
         logger.exception(ex)
+        raise ex
 
 
 if __name__ == "__main__":
